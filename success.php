85a8431a2ddd1a25aa4103b3a2dc53722db28949
Aya Osama <aya55osma2001@gmail.com>
2022-03-26 22:45:54 +0200

    success page

--- success.php
+++ success.php
@@ -6,4 +6,27 @@
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet" href="css/style2.css">
     </head>
+    <body>
+        <div class="success">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="image">
+                        <!-- <img class="img-responsive" src="imgs/undraw_Accept_terms_re_lj38.png"> -->
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="success-content">
+                        <div class="text">
+                            <h1>Great :)</h1>
+                            <p>You are a free person from infection and you can enter freely !.</p>
+                            <a class="back-main" href="index.php">Back to Main Page...</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="js/jquery-3.6.0.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/file.js"></script>
+    </body>
 </html>
